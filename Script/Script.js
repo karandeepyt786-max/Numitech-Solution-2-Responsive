0104add99e486d2330e1406c289c6879679c681e
Karan <Karan@gmail.com>
2026-02-14 17:04:59 +1200

    Numtitech updates 2

---
+++ Script/Script.js
@@ -0,0 +1,48 @@
+let Left=document.getElementsByClassName("main5CovererContainer2LeftArrowSlide")[0]
+let Right=document.getElementsByClassName("main5CovererContainer2RightArrowSlide")[0]
+let CountingBoxes=document.getElementsByClassName("main5CovererContainer2")[0]
+let AllCount=CountingBoxes.querySelectorAll(".main5CovererContainer2Box1")
+let OnlyBox1=document.getElementsByClassName("main5CovererContainer2Box1")[0]
+let OnlyBox2=document.getElementsByClassName("main5CovererContainer2Box1")[1]
+let OnlyBox3=document.getElementsByClassName("main5CovererContainer2Box1")[2]
+
+let index=0
+
+Right.addEventListener("click",()=>{
+    console.log("Left click")
+    console.log(index)
+
+if(index<=5 ){
+   index++
+    
+    AllCount.forEach((ite)=>{
+        ite.style.transform=`translateX(-${index*100}%)`
+        ite.style.transition="3s"
+    })
+    console.log(index)
+}
+else{
+            index--
+    console.log(index)
+ 
+}
+
+})
+
+
+Left.addEventListener("click",()=>{
+    console.log("Left click")
+    console.log(index)
+
+if(index>=5 ){
+   index--
+    
+    AllCount.forEach((ite)=>{
+        ite.style.transform=`translateX(${index*100}%)`
+        ite.style.transition="3s"
+    })
+    console.log(index)
+}
+
+
+})
